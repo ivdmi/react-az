775f27c9b4b9741d3cde8200ee3321d24f2077e2
DmIrina <dmytriieva.iryna@gmail.com>
2022-12-06 22:12:40 +0200

    input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "../src/styles/App.css"
 import {useState} from "react";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -16,6 +17,15 @@ function App() {
         {id: 3, title: 'C#', body: 'C# - Description'},
     ])
 
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+
+    const addNewPost = (e) => {
+        e.preventDefault()      // відключити Submit - не оновлювати сторінку коли тиснемо на кнопку
+        console.log(title)      // дані з управляемого інпута
+        console.log(body)      // дані з управляемого інпута
+    }
+
     return (
         <div className="App">
             <Input/>
@@ -24,9 +34,19 @@ function App() {
             <h3>---------------------------------End Tests-----------------------------------</h3>
 
             <form>
-                <input type="text" placeholder="Назва поста"/>
-                <input type="text" placeholder="Опис поста"/>
-                <MyButton disabled>Створити пост</MyButton>
+                {/* Управляемий компонент */}
+                <MyInput type="text"
+                         value={title}
+                         onChange={e => setTitle(e.target.value)}
+                         placeholder="Назва поста"/>
+
+                {/* управляемий компонент */}
+                <MyInput
+                    value={body}
+                    onChange={e => setBody(e.target.value)}
+                    type="text" placeholder="Опис поста"
+                />
+                <MyButton onClick={addNewPost}>Створити пост</MyButton>
             </form>
 
             <PostList posts={posts} title="Список постів - 1"/>
