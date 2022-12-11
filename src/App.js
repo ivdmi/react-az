e961ddd8e3adb62a5e27df8cac270973b9a035cd
DmIrina <dmytriieva.iryna@gmail.com>
2022-12-11 11:32:32 +0200

    +hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Counter from "./components/Counter";
 import Input from "./components/Input";
 import "../src/styles/App.css"
-import {useMemo, useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -17,19 +17,19 @@ function App() {
 
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
-
-
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
     // useMemo пвиконує обчислення (сортування), запам"ятовує та кешує результат. Виконується коли змінюється масив залежностей
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort], 'uk'))
-        }
-        return posts;
-    }, [filter.sort, posts])
+    // const sortedPosts = useMemo(() => {
+    //     if (filter.sort) {
+    //         return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort], 'uk'))
+    //     }
+    //     return posts;
+    // }, [filter.sort, posts])
 
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter((post => post.title.toLowerCase().includes(filter.query.toLowerCase())))
-    }, [filter.query, sortedPosts])
+    // const sortedAndSearchedPosts = useMemo(() => {
+    //     return sortedPosts.filter((post => post.title.toLowerCase().includes(filter.query.toLowerCase())))
+    // }, [filter.query, sortedPosts])
+
 
 
     // callback - функція необхідна для отримання параметра  newPost з нижнього компонента
